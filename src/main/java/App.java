4418c2876d72e583a0bb1cb19ecaff46672525a7
Esvethlynna Pangelinan <esveth@aol.com>
2017-07-07 11:40:29 -0700

    update with RESTful Route code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,27 +13,40 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("members", request.session().attribute("members"));
+      //model.put("members", request.session().attribute("members"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/members", (request, response) -> {
+    get("members/new", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/member-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
-      ArrayList<Member> members = request.session().attribute("members");
-      if (members == null) {
-        members = new ArrayList<Member>();
-        request.session().attribute("members", members);
-      }
+    get("/members", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("members", Member.all());
+      model.put("template", "templates/members.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
+    post("/members", (request,response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
       String memberName = request.queryParams("memberName");
       Member newMember = new Member(memberName);
-      members.add(newMember);
-
       model.put("template", "templates/success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
+
+    get("/members/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Member member = Member.find(Integer.parseInt(request.params(":id")));
+      model.put("member", member);
+      model.put("template", "templates/member.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
 
   }
 }
