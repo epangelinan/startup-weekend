f67e0277b3f13c47f9e9bc2b6be62fb3b0ebdbde
Esvethlynna Pangelinan <esveth@aol.com>
2017-07-07 10:53:18 -0700

    add code for date and time, retrieving list of members, clearing the list of members, assigning unique IDs, and tests

--- src/main/java/Member.java
+++ src/main/java/Member.java
@@ -1,12 +1,48 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Member {
   private String mMemberName;
+  private boolean mCompleted;
+  private LocalDateTime mCreatedAt;
+  private static List<Member> instances = new ArrayList<Member>();
+  private int mId;
 
   public Member(String memberName) {
     mMemberName = memberName;
+    mCompleted = false;
+    mCreatedAt = LocalDateTime.now();
+    instances.add(this);
+    mId = instances.size();
   }
 
   public String getName() {
     return mMemberName;
+  }
+
+  public boolean isCompleted() {
+    return mCompleted;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return mCreatedAt;
+  }
+
+  public static List<Member> all() {
+    return instances;
+  }
+
+  public static void clear() {
+    instances.clear();
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static Member find(int id) {
+    return instances.get(id - 1);
   }
 
 }
--- src/test/java/MemberTest.java
+++ src/test/java/MemberTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 public class MemberTest {
 
@@ -15,4 +16,44 @@ public class MemberTest {
     assertEquals("Anthony", myMember.getName());
   }
 
+  @Test
+  public void isCompleted_isFalseAfterInstantiation_false() {
+    Member myMember = new Member("Anthony");
+    assertEquals(false, myMember.isCompleted());
+  }
+
+  @Test
+  public void getCreatedAt_instantiatesWithCurrentTime_today() {
+    Member myMember = new Member("Anthony");
+    assertEquals(LocalDateTime.now().getDayOfWeek(), myMember.getCreatedAt().getDayOfWeek());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfMember_true() {
+    Member firstMember = new Member("Anthony");
+    Member secondMember = new Member("Henry");
+    assertEquals(true, Member.all().contains(firstMember));
+    assertEquals(true, Member.all().contains(secondMember));
+  }
+
+  @Test
+  public void clear_emptiesAllMembersFromArrayList_0() {
+    Member myMember = new Member("Anthony");
+    Member.clear();
+    assertEquals(0, Member.all().size());
+  }
+
+  @Test
+  public void getId_membersInstantiateWithAnID_1() {
+    Member.clear();  // Remember, the test will fail without this line! We need to empty leftover Tasks from previous tests!
+    Member myMember = new Member("Anthony");
+    assertEquals(1, myMember.getId());
+  }
+
+  @Test
+  public void find_returnsMemberWithSameId_secondMember() {
+    Member firstMember = new Member("Anthony");
+    Member secondMember = new Member("Henry");
+    assertEquals(Member.find(secondMember.getId()), secondMember);
+  }
 }
